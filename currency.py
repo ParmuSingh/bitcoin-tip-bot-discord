848f356a15cff210303bd96663348c28189c2bf7
Paramdeep Singh Obheroi <p.s.obheroi@gmail.com>
2019-08-29 17:18:16 +0530

    no need for servers. removed ambiguity of usernames by using IDs.

--- currency.py
+++ currency.py
@@ -1,72 +1,83 @@
 import json
+import pickle
 
 #####################
-# balance = {		#
-# 	'tag' : 'bal'	#
-# }					#
+# balance = {       #
+#   'tag' : 'bal'   #
+# }                 #
 #####################
 class Currency:
-	def __init__(self, server):
-		self.server = server
-		self.balances_file = open(f"./balances.json", "r+")
+	def __init__(self):
+		# self.server = server
+		# self.balances_file = open(f"./balances.json", "r+")
+		# self.balances_file = open(f"./balances.json", "r+")
 		self.balances = {}
-		self.member_list = []
+
+		# self.members = {}
+		self.members_list = []
 		self.sat_reserve = 100
-		for member in server.members:
-			if not member.bot:
-				self.member_list.append(str(member).split('#')[1])
-		
+
+		# for member in server.members:
+		#   if not member.bot:
+		#       self.member_list.append(member.split('#')[1])
+		# with open("./balances.json", "r+") as balances_file:
+		# 	self.balances = json.load(balances_file)
 		try:
-			self.balances = json.load(self.balances_file)
-			# self.member_list = [str(member).split("#")[1] for member in server.members]
+			with open("./balances.json", "r+") as balances_file:
+				self.balances = json.load(balances_file)
+		# 	# self.balances = json.load(self.balances_file)
+		# 	print(self.balances)
+		# 	# self.member_list = [member.split("#")[1] for member in server.members]
 		except:
-			print("balance file doesn't exist, creating..")
-			for member in member_list:
-				self.balances[member] = 0
+			print("balance file is empty..")
 			self.update_balance_file()
 
+		# self.balances = json.load(self.balances_file)
+		self.members_list = list(self.balances.keys())
+		print(f"members = {self.members_list}")
+		# self.members = json.load()
 		# print(self.member_list) # includes bots
-	def show_balance(self):
-		return self.balances
+	def show_balance(self, id):
+		return self.balances[id]
 
-	def do_transaction(self, sender_name, reciever_name, amount):
+	def do_transaction(self, sender_name, sender_id, reciever_name, reciever_id, amount):
+		print("test")
+		print(amount)
 		amount = int(amount)
 		print(f"initiating tx: {sender_name} -> {reciever_name} | amount = {amount}")
 		
-		valid, err = self.is_tx_valid(sender_name, reciever_name, amount)
+		if sender_id not in self.members_list:
+			print('sender not in members_list')
+			self.balances[sender_id] = 0
+			self.members_list.append(sender_id)
+			self.update_balance_file()
+		if reciever_id not in self.members_list:
+			print('recv not in members_list')
+			self.balances[reciever_id] = 0
+			self.members_list.append(reciever_id)
+			self.update_balance_file()
+
+
+		valid, err = self.is_tx_valid(sender_id, reciever_id, amount)
 
 		if valid:
-			self.balances[self.get_tag(reciever_name)]	+= amount
-			self.balances[self.get_tag(sender_name)] 	-= amount
+			self.balances[reciever_id]  += amount
+			self.balances[sender_id]    -= amount
 			self.update_balance_file()
 			return True, "none"
 		else:
 			return False, err
-	def is_tx_valid(self, sender, receiver, amt):
-		if self.balances[self.get_tag(sender)] <= int(amt):
+	def is_tx_valid(self, sender_id, receiver_id, amt):
+		if self.balances[sender_id] < int(amt):
 			return False, "not enough funds"
 		
-		receiver_exists = False
-		# checking if name exists
-		for member in self.server.members:
-			if receiver.lower() == member.name.lower():
-				receiver_exists = True
-				break
-		if not receiver_exists:
-			return False, "recipeint_doesn't exist"
-
-		if self.get_tag(receiver) not in self.member_list:
-			return False, "recipeint doesn't exist"
-		
 		return True, "none"
-	def get_tag(self, name):
-		for member in self.server.members:
-			if str(member).split('#')[0].lower() == name.lower():
-				# print(str(member).split('#')[1])
-				return str(member).split('#')[1]
 	def update_balance_file(self):
-		self.balances_file.seek(0)
-		json.dump(self.balances, self.balances_file)
-		self.balances_file.truncate()
+		with open("./balances.json", "w") as balances_file:
+			json.dump(self.balances, balances_file)
+		# self.balances_file.seek(0)
+		# json.dump(self.balances, self.balances_file)
+		# self.balances_file.truncate()
 	def close_file(self):
-		self.balances_file.close()
+		# self.balances_file.close()
+		pass
--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 #discord.__version__ = 1.0.0a
-client_id_file 	= open("./private_data/client_id.txt", "r")
-token_file 		= open("./private_data/token.txt", "r")
-server_id_file 	= open("./private_data/my_server_id.txt", "r")
-client_id 		= int(client_id_file.read())
-server_id 		= int(server_id_file.read())
-token 			= token_file.read()
-permissions 	= 68608
+client_id_file 		= open("./private_data/client_id.txt", "r")
+token_file 			= open("./private_data/token.txt", "r")
+server_id_file 		= open("./private_data/my_server_id.txt", "r")
+# admin_secret_file	= open("./private_data/my_admin_secret.txt", "r")
+client_id 			= int(client_id_file.read())
+server_id 			= int(server_id_file.read())
+# admin_secret 		= admin_secret_file.read()
+token 				= token_file.read()
+permissions 		= 68608
 
 # https://discordapp.com/oauth2/authorize?client_id={CLIENT_ID}&scope=bot&permissions={PERMISSIONSINT}
 add_to_server_url = f"https://discordapp.com/oauth2/authorize?client_id={client_id}&scope=bot&permissions={permissions}"
@@ -13,6 +15,7 @@ print(add_to_server_url)
 
 import discord
 import currency
+# from Crypto.Hash import SHA256
 
 # initiates client
 client = discord.Client()
@@ -23,11 +26,15 @@ async def on_ready(currency=currency):
 
 	global currency_lastbench
 
-	server = client.get_guild(id=server_id) # Eishhtimulation
+	# server = client.get_guild(id=server_id) # Eishhtimulation
 	
-	currency_lastbench = currency.Currency(server)
+	currency_lastbench = currency.Currency()
 	# currency_lastbench.show_balance()
 
+# @bot.command()
+# async def getuser(ctx, userid: int):
+#     user = bot.get_user(userid)
+#     await ctx.send(user.name)
 
 @client.event
 async def on_message(message):
@@ -38,33 +45,50 @@ async def on_message(message):
 		currency_lastbench.close_file()
 		await client.close()
 		return
-	if "!btctip show all balances" in message.content.lower() and message.author.name!="bitcoin-tip-bot":
-		await message.channel.send(currency_lastbench.show_balance())
+	if "!btctip show my balance" in message.content.lower() and message.author.name!="bitcoin-tip-bot":
+		await message.channel.send(f"you have {currency_lastbench.show_balance(str(message.author.id))}sats")
 		return
 	if "!btctip help" in message.content.lower() and message.author.name!="bitcoin-tip-bot" :
-		await message.channel.send("hey hey heyyyyy I'm a bitcoin bot you can use to tip people within this server with some bitcoin. My creator has envisoned me to work with bitcoin's lightning network so you can do microtransaction instantly.\n\nRight now you can only send to people within this server but my creator will soon add the feature to withdraw your bitcoin which you can receive on any bitcoin wallet.\n\nTo send money:\n\n!btctip <username-of-receiver> <amt-in-satoshi>\n 	Example: !btctip arpan 50\n 			This will send 50 satoshis from your wallet to arpan's wallet.\n\nTo see all balances:\n\n!btctip show all balances\n\n\nps: 1 satoshi is 0.00000001 bitcoin 		|		 I may not be online all the time.\n\nbeep beep boop boop - created by a smart ass.")
+		await message.channel.send("hey hey heyyyyy I'm a bitcoin bot you can use to tip people within this server with some bitcoin. My creator has envisoned me to work with bitcoin's lightning network so you can do microtransaction instantly.\n\nRight now you can only send bitcoin already in circulation but my creator will soon add the feature to withdraw (and deposit) your bitcoin which you can receive on any bitcoin wallet.\n\nTo send money:`!btctip <mention-username-of-receiver> <amt-in-satoshi>`\nExample: `!btctip @ParmuSingh#5099 50` This will send 50 satoshis from your wallet to ParmuSingh's wallet.\n\nTo see your balances:\n`!btctip show my balance`\n\n\nps: 1 satoshi is 0.00000001 bitcoin 		|		 I may not be online all the time.\n\nbeep beep boop boop - created by a smart ass.")
 		return
 	if "!btctip" in message.content.lower() and message.author.name!="bitcoin-tip-bot":
 		try:
 			words = message.content.lower().split(' ')
-
-			sender = message.author.name
+			# print(words)
+			# sometimes mentioning user adds another empty char which could fuck things up so I'm removing any spaces registered as words:
+			for i in range(len(words)):
+				if words[i] == '':
+					words.pop(i)
+					break
+			# print(words)
+			# sender_name = message.author.name
 			for i in range(len(words)):
 				if words[i] == "!btctip":
 					recipient = words[i+1]
 					amount = words[i+2]
+					break
 					# not returning if invalid after invoke command cuz it raises exception and program goes on
-			if sender.lower() == recipient.lower():
-				await message.channel.send("why do would you tip yourself...?")
+			# print(f"userID = {recipient[2:len(recipient) - 1]}")
+			# print(f"username = {client.get_user(int(recipient[2:len(recipient) - 1]))}")
+			
+			sender_id = str(message.author.id)
+			sender_name = message.author.name
+			recipient_id = str(recipient[2:len(recipient) - 1])
+			recipient_name = client.get_user(recipient_id)
+
+			print(f"sender_name = {sender_name} | sender_id = {sender_id} | recipient_name = {recipient_name} | recipient_id = {recipient_id}")
+
+			if sender_id == recipient_id:
+				await message.channel.send("why would you tip yourself...?")
 				return
 			# sender_tag = message.author.tag
 			# recipient = words[1]
 			# amount = words[2]
-			success, err = currency_lastbench.do_transaction(sender, recipient, amount)
+			success, err = currency_lastbench.do_transaction(sender_name, sender_id, recipient_name, recipient_id, amount)
 			if success:
-				await message.channel.send(f"hey heyy heyyy\n{amount} satoshis sent to {recipient} from {sender}\n\n1 satoshi = 0.00000001 bitcoin")
+				await message.channel.send(f"hey heyy heyyy\n{amount} satoshis sent to {str(recipient_name).split('#')[0]} from {sender_name}\n\n1 satoshi = 0.00000001 bitcoin")
 			else:
-				await message.channel.send(f"uh there's some problem: {err}")
+				await message.channel.send(f"problem: {err}")
 
 		except ValueError as e:
 			print(e)
